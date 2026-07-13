762e450e42c0a6ab6ac4689c9762e484d8345485
zaidmukaddam <zaidaltafmukaddam@gmail.com>
2026-07-13 17:56:36 +0530

    enable vercel analytics

--- site/app/layout.tsx
+++ site/app/layout.tsx
@@ -1,4 +1,6 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './global.css';
 import { Google_Sans_Flex } from 'next/font/google';
 
@@ -18,6 +20,8 @@ export default function Layout({ children }: LayoutProps<'/'>) {
     >
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
